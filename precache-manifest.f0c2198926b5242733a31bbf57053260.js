ea91b3d66e91ab3c8ed965db04d62b89ba210b4e
Tsiresy RABEKOTO <tsiresyrabekoto@gmail.com>
2021-01-03 23:23:59 +0100

    Updates

---
+++ precache-manifest.f0c2198926b5242733a31bbf57053260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821f0979103a6d550b4132a18de29e84",
+    "url": "/file-test/index.html"
+  },
+  {
+    "revision": "e52942194ebe1da0c8dd",
+    "url": "/file-test/static/css/main.d1eed238.chunk.css"
+  },
+  {
+    "revision": "fff06567c165c609f926",
+    "url": "/file-test/static/js/2.ee2d21fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/file-test/static/js/2.ee2d21fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52942194ebe1da0c8dd",
+    "url": "/file-test/static/js/main.a305f879.chunk.js"
+  },
+  {
+    "revision": "8cb800dd17f9dbcf73b3",
+    "url": "/file-test/static/js/runtime-main.2b3048a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/file-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
